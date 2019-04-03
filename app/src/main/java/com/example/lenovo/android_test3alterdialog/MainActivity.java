45782216f47461d75cc31f0737c5332b32ea22f1
w814698066 <814698066@qq.com>
2019-04-03 10:59:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/android_test3alterdialog/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lenovo.android_test3alterdialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TableLayout tableLayout = (TableLayout) getLayoutInflater().inflate(R.layout.activity_main,null);
+        new AlertDialog.Builder(this).setView(tableLayout).create().show();
+    }
+}
